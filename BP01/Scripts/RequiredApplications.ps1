2f221ee61a83198e39af26f4174a16a328221aab
listlfa <list_lfa@yahoo.com>
2015-09-11 12:47:28 +1000

    a

---
+++ BP01/Scripts/RequiredApplications.ps1
@@ -0,0 +1,7 @@
+
+Invoke-WebRequest https://downloads.puppetlabs.com/windows/puppet-3.5.1.msi -OutFile ..\Files-Apps\puppet-3.5.1.msi
+Invoke-WebRequest https://github-windows.s3.amazonaws.com/GitHubSetup.exe -OutFile ..\Files-Apps\GitHubSetup.exe
+
+..\Files-Apps\puppet-3.5.1.msi
+..\Files-Apps\GitHubSetup.exe
+
